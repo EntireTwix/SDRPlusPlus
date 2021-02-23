c6a68e3a85722a31f51a8493314c75511e8d05a1
William Katz <willkatz.dev@gmail.com>
2021-02-23 12:23:30 -0800

    const ref string and unordered map changes

--- core/src/module.h
+++ core/src/module.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <string>
-#include <map>
+#include <string_view>
+#include <unordered_map>
 #include <json.hpp>
 
 #ifdef _WIN32
@@ -35,14 +35,14 @@ public:
         const int maxInstances;
     };
 
-    class Instance {
-    public:
+    struct Instance {
         virtual void enable() = 0;
         virtual void disable() = 0;
         virtual bool isEnabled() = 0;
     };
 
-    struct Module_t {
+    struct Module_t
+    {
 #ifdef _WIN32
         HMODULE handle;
 #else
@@ -51,41 +51,36 @@ public:
         ModuleManager::ModuleInfo_t* info;
         void (*init)();
         ModuleManager::Instance* (*createInstance)(std::string name);
-        void (*deleteInstance)(ModuleManager::Instance* instance);
+        void (*deleteInstance)(ModuleManager::Instance *instance);
         void (*end)();
 
-        friend bool operator==(const Module_t& a, const Module_t& b) {
-            if (a.handle != b.handle) { return false; }
-            if (a.info != b.info) { return false; }
-            if (a.init != b.init) { return false; }
-            if (a.createInstance != b.createInstance) { return false; }
-            if (a.deleteInstance != b.deleteInstance) { return false; }
-            if (a.end != b.end) { return false; }
-            return true;
+        friend bool operator==(const Module_t &a, const Module_t &b) {
+            //branchless
+            return (a.handle != b.handle) * (a.info != b.info) * (a.init != b.init) * (a.createInstance != b.createInstance) * (a.deleteInstance != b.deleteInstance) * (a.end != b.end);
         }
     };
 
-    struct Instance_t {
+    struct Instance_t
+    {
         ModuleManager::Module_t module;
         ModuleManager::Instance* instance;
     };
 
-    ModuleManager::Module_t loadModule(std::string path);
+    ModuleManager::Module_t loadModule(const std::string &path);
 
-    void createInstance(std::string name, std::string module);
-    void deleteInstance(std::string name);
-    void deleteInstance(ModuleManager::Instance* instance);
+    void createInstance(const std::string &name, const std::string &module);
+    void deleteInstance(const std::string &name);
+    void deleteInstance(ModuleManager::Instance *instance);
 
-    void enableInstance(std::string name);
-    void disableInstance(std::string name);
-    bool instanceEnabled(std::string name);
+    void enableInstance(const std::string &name);
+    void disableInstance(const std::string &name);
+    bool instanceEnabled(const std::string &name);
 
-    int countModuleInstances(std::string module);
+    int countModuleInstances(const std::string &module) const;
 
 private:
-    std::map<std::string, ModuleManager::Module_t> modules;
-    std::map<std::string, ModuleManager::Instance_t> instances;
-
+    std::unordered_map<std::string, ModuleManager::Module_t> modules;
+    std::unordered_map<std::string, ModuleManager::Instance_t> instances;
 };
 
-#define SDRPP_MOD_INFO    MOD_EXPORT const ModuleManager::ModuleInfo_t _INFO_
+#define SDRPP_MOD_INFO MOD_EXPORT const ModuleManager::ModuleInfo_t _INFO_
