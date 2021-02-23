dbc1ee2e810114fc49c1e73acc6e09dc259fe4af
William Katz <willkatz.dev@gmail.com>
2021-02-23 12:19:23 -0800

    removed redundant #include <module.h>

--- core/src/core.h
+++ core/src/core.h
@@ -2,9 +2,9 @@
 #include <config.h>
 #include <module.h>
 #include <scripting.h>
-#include <module.h>
 
-namespace core {
+namespace core
+{
     SDRPP_EXPORT ConfigManager configManager;
     SDRPP_EXPORT ScriptManager scriptManager;
     SDRPP_EXPORT ModuleManager moduleManager;
